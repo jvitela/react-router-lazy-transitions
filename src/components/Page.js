0c64563ac2fdeb85ffee02877f160dbc0f1292c9
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-04-10 21:28:58 +0200

    Added logic for scroll restoration

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,18 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
 
-export const Page = ({ className, scrollTop, children }) => {
-  useEffect(() => {
-    if (scrollTop) {
-      window.scrollTo(0, 0);
-    }
-  }, [scrollTop]);
+const PageCache = {};
+
+export const Page = ({ className, children }) => {
+  const pathname = useInitialPathname();
+  const elemRef = useRef();
+
+  useEffect(
+    function restoreScrollPosition() {
+      const elem = elemRef.current;
+      if (pathname in PageCache) {
+        elem.scrollTop = PageCache[pathname];
+      }
+    },
+    [pathname, elemRef]
+  );
+
+  const saveScrollPosition = () => {
+    PageCache[pathname] = elemRef.current.scrollTop;
+  };
+
   return (
-    <div className="w-full">
+    <div
+      ref={elemRef}
+      className="absolute left-0 top-0 right-0 bottom-0 overflow-auto"
+      onScroll={saveScrollPosition}
+    >
       <div
-        className={`max-w-screen-lg mx-auto py-6 px-6 md:px-12 bg-white ${className}`}
+        className={`max-w-screen-xl mx-auto py-6 px-6 md:px-12 bg-white ${className}`}
       >
         {children}
       </div>
     </div>
   );
 };
+
+/**
+ * Returns the pathname at the moment the component was
+ *  mounted into the page.
+ */
+function useInitialPathname() {
+  const location = useLocation();
+  const ref = useRef(location.pathname);
+  return ref.current;
+}
